92b1983e00680b2b477c0c8fb2792d092f24767d
Michael Slowik <slowik.michael@gmail.com>
2021-10-25 14:47:58 -0500

    Option to set alternative header for authentication

--- config/artisan.php
+++ config/artisan.php
@@ -1,9 +1,16 @@
 <?php
 
 return [
+    // Prefix for routes used to make artisan calls.
     'route' => [
         'prefix' => '_artisan',
     ],
+
+    // Token used for authentication.
+    'token_header' => 'x-GCP-token',
+
+    // Queue connection.
     'connection' => env('ARTISAN_CONNECTION', env('QUEUE_CONNECTION', 'cloudtasks')),
+    // Queue name.
     'queue' => env('ARTISAN_QUEUE', 'artisan'),
 ];
